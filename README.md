8508a35dc89dddb5f86633f7e41780a331773222
ACubero <alexcuar@gmail.com>
2024-03-31 16:35:47 +0200

    Creació de la api

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Una api sencilla hecha con Pytho + Fastapi
 
 Siguiendo las indicaciones del video
 https://www.youtube.com/watch?v=J0y2tjBz2Ao
+
+1. pip install fastapi
+2. pip install uvicorn
+3. Hacer el programa
+4. Para ejecutar, primero levantar el servicio con la instruccion "uvicorn <nombre_programa>:<nombre_app> --reload"
+Ejemplo: uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+libro = {}
+
+class Libro(BaseModel):
+    id: int
+    titulo: str
+    escritor: str
+    capitulos: Optional[int]
+
+def crearLibro(id,titulo,escritor,capitulos):
+    libro[id] = {"id":id,"titulo":titulo,"escritor":escritor,"capitulos":capitulos}
+
+crearLibro(1,"Genesis","Moises",67)
+crearLibro(2,"Exodo","Moises",43)
+crearLibro(3,"Levitico","Moises",21)
+print(libro)
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return libro
+
+@app.get("/libros/{id}")
+def detalleLibro(id: int):
+    return libro[id]
+
+#Para hacer un post
+"""@app.post("/libros")
+def insertarLibro(libro:Libro):
+    return {"message":f"Libro {libro.titulo} insertado"}"""
